a38238bdb71a87ca196ecc189e24f1e77d687cfa
kphi <kphi@ecg-hq.com>
2020-11-04 14:48:54 -0700

    update

--- pip-package/setup.py
+++ pip-package/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='pip-package',
     version='0.0.1',
-    description='Hello world pip package from private github repo',
+    description='My private package from private github repo',
     url='git@github.com:KittPhi/pip-package.git',
-    author='Kitt Phi',
-    author_email='kittphi@gmail.com',
+    author='Raphael Schubert',
+    author_email='raphael.schubert@digitalbankscorp.com',
     license='unlicense',
     packages=['pip-package'],
     zip_safe=False
