336fcbe1f1627208aa59de32fae9c24430a789f9
kphi <kphi@ecg-hq.com>
2020-11-04 15:03:19 -0700

    edit comment.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='pip-package',
+    name='pippackage',
     version='0.0.1',
     description='Hello world pip package from private github repo',
     url='https://github.com/KittPhi/pip-package.git',
     author='Kitt Phi',
     author_email='kittphi@gmail.com',
     license='unlicense',
-    packages=['pip-package'],
+    packages=['pippackage'],
     zip_safe=False
 )
