6291ff25a44ad548ec6481af7492ce638294153c
kphi <kphi@ecg-hq.com>
2020-11-04 15:58:32 -0700

    rename

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='pippackage',
+    name='hello_world',
     version='0.0.1',
     description='Hello world pip package from private github repo',
     url='https://github.com/KittPhi/pip-package.git',
     author='Kitt Phi',
     author_email='kittphi@gmail.com',
     license='unlicense',
-    packages=['pippackage'],
+    packages=['hello_world'],
     zip_safe=False
 )
